0f2bb3122263911447b1ade2aeeb67f7ccfa4663
Ovar9000 <quibotmark@gmail.com>
2026-09-01 18:38:15 +0800

    feat(scanner): enable HTTPS support and add direct mobile camera shutter photo capture fallback

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import url from 'url';
 
 // Vite dev server API simulation plugin to test /api endpoints locally
@@ -93,6 +94,7 @@ export default defineConfig({
   base: '/',
   plugins: [
     react(),
+    basicSsl(),
     devApiPlugin(),
     VitePWA({
       registerType: 'autoUpdate',
